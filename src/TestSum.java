918425a05051440b7c3d72df0ea5674f8fcf28d1
Nicolas Lorenz Rosado <Nicolas Lorenz Rosado>
2016-10-26 16:02:11 +0200

    Segundo commit

--- src/TestSum.java
+++ src/TestSum.java
@@ -8,6 +8,13 @@ public class TestSum {
 	public void testSum2Num(){
 		assertEquals(6,sum(4,2));
 	}
+	
+	@Test
+	public void testSumWithZero(){
+		assertEquals(3,sum(3,0));
+		assertEquals(3,sum(0,3));
+	}
+	
 	private int sum(int a,int b){
 		return a+b;
 	}
